0f3f1cb0076b5ccfc71ad2aa880bde450aab9290
Edwin Guerrero <edwinguerrero001@gmail.com>
2020-01-18 11:44:05 -0700

    Updated google maps versions, and added uses-feature in AndroidManifest xml file

--- app/src/main/java/com/example/silenceapp/MapsActivity.java
+++ app/src/main/java/com/example/silenceapp/MapsActivity.java
@@ -3,12 +3,15 @@ package com.example.silenceapp;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
